fd9e41733d366f854d9d1b211653df9c18c8aeca
卢国祥 <noob-coder@qq.com>
2026-01-17 08:58:59 +0800

    feat(hm_symbol): 新增 HarmonyOS Symbol 图标库
    - 集成了近 400 个 HarmonyOS 系统图标
    - 实现了自动生成图标代码的工具脚本
    - 配置了字体文件和相关依赖
    - 添加了完整的项目文档和使用说明
    - 设置了代码检查和构建配置
    - 提供了图标使用的 API 接口

---
+++ README.md
@@ -0,0 +1,29 @@
+## HarmonyOS Symbol
+
+[HarmonyOS Symbol](https://developer.huawei.com/consumer/cn/design/harmonyos-symbol/) 代表了 HarmonyOS 系统图标设计的一次重要演进。
+
+它通过简化的图形和直观的语义表达，不仅易于识别，同时融入了更多年轻化的设计理念，使得整体的视觉风格更加年轻和时尚。
+
+同时 HarmonyOS Symbol 还具备了字体属性，使其能够与系统字体无缝对接，确保了文本与图标的排版一致性。
+
+此外，它能够根据文本的粗细变化进行智能适配，确保了在不同文本粗细下，图标能够灵活适应，保持视觉的连贯性和一致性。
+
+![icons.png](./icons.png)
+
+## Usage
+
+Use with the [Icon](https://api.flutter.dev/flutter/widgets/Icon-class.html) class to show specific icons.
+
+Icons are identified by their name as listed below, e.g.
+
+[HarmonySymbols.HarmonyOS_Next].
+
+```dart
+import 'package:hm_symbol/hm_symbol.dart';
+
+Icon(HarmonySymbols.HarmonyOS);
+```
+
+## Design Guides
+
+https://developer.huawei.com/consumer/cn/doc/design-guides/system-icons-0000001929854962
---
+++ tool/generate.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+
+import 'package:code_builder/code_builder.dart';
+import 'package:dart_style/dart_style.dart';
+
+const fontFamily = 'HM Symbol';
+const fontPackage = 'hm_symbol';
+
+const url = '';
+
+void main() {
+  List<List<String>> cmap = File('./tool/HMSymbol.nam')
+      .readAsStringSync()
+      .split('\n')
+      .map((line) => line.trim()) // 去除每行首尾空白
+      .where((line) => line.isNotEmpty) // 过滤空行
+      .map((line) => line.split(' ')) // 按单个空格分割
+      .toList();
+
+  generate(cmap);
+}
+
+const header =
+    '''
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// 
+// **************************************************************************
+// $fontPackage
+// **************************************************************************
+//
+''';
+
+void generate(List<List<String>> icons) {
+  final library = LibraryBuilder()
+    ..directives.add(Directive.import('package:flutter/widgets.dart'))
+    ..body
+    ..comments.add(header)
+    ..body.addAll([
+      (ClassBuilder()
+            ..docs.addAll([
+              '/// ',
+              '/// Use with the [Icon] class to show specific icons. Icons are identified by their name as listed below, e.g. ',
+              '/// [HarmonySymbols.HarmonyOS_Next].',
+              '/// ',
+            ])
+            ..annotations.add(refer('staticIconProvider'))
+            ..name = 'HarmonySymbols'
+            ..abstract = true
+            ..modifier = ClassModifier.final$
+            ..fields.addAll([
+              (FieldBuilder()
+                    ..static = true
+                    ..modifier = FieldModifier.constant
+                    ..name = 'iconFont'
+                    ..assignment = literalString(fontFamily).code)
+                  .build(),
+
+              (FieldBuilder()
+                    ..static = true
+                    ..modifier = FieldModifier.constant
+                    ..name = 'iconFontPackage'
+                    ..assignment = literalString(fontPackage).code)
+                  .build(),
+
+              for (final icon in icons)
+                (FieldBuilder()
+                      ..static = true
+                      ..modifier = FieldModifier.constant
+                      ..type
+                      ..name = icon[1]
+                      ..assignment = refer('IconData')
+                          .newInstance(
+                            [literalNum(int.parse(icon[0]))],
+                            {
+                              'fontFamily': refer('iconFont'),
+                              'fontPackage': refer('iconFontPackage'),
+                            },
+                          )
+                          .code)
+                    .build(),
+            ]))
+          .build(),
+    ]);
+
+  final code =
+      DartFormatter(
+        pageWidth: 120,
+        languageVersion: DartFormatter.latestLanguageVersion,
+      ).format(
+        DartEmitter(
+          orderDirectives: true,
+          useNullSafetySyntax: true,
+        ).visitLibrary(library.build()).toString(),
+      );
+
+  File('./lib/src/assets.g.dart').writeAsStringSync(code);
+}
